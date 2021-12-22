3338800dcdba49be9ac64de9d0ac4e1ca1481795
youngmo <youngmo>
2021-12-22 23:30:50 +0900

    study Coding

---
+++ iOS_30Project-Ch.08_Corona19/CovidDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CobidDetailViewController.swift
+//  iOS_30Project-Ch.08_Corona19
+//
+//  Created by youngmo on 2021/12/22.
+//
+
+import UIKit
+
+class CovidDetailViewController: UITableViewController {
+
+    @IBOutlet weak var newCaseCell: UILabel!
+    @IBOutlet weak var totalCaseCell: UILabel!
+    @IBOutlet weak var recoveredCell: UILabel!
+    @IBOutlet weak var deathCell: UILabel!
+    @IBOutlet weak var percentageCell: UILabel!
+    @IBOutlet weak var overseasInflowCell: UILabel!
+    @IBOutlet weak var regionalOutbreakCell: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
